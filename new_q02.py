bbde40ea46e6098d4fb96f8e6935ff68d878e110
Dario Vitoriano <dariovitoriano2771@gmail.com>
2022-05-25 19:39:23 -0400

    Create new_q02.py

---
+++ new_q02.py
@@ -0,0 +1,5 @@
+numero1=int(input('valor decimal: '))
+hexa=int(input('valor hexa: '))
+octal=int(input('valor oct: '))
+print(f"numero em hexadecimal , {hexa}")
+print(f"numero em octal , {octal}")
