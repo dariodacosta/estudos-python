d02eb4263ddc5385f8938b344e388a3908892970
Dario Vitoriano <dariovitoriano2771@gmail.com>
2022-05-26 17:27:00 -0400

    Update new_q02.py

--- new_q02.py
+++ new_q02.py
@@ -1,3 +1,6 @@
 numero1=int(input('valor decimal: '))
-hexa=int(input('valor hexa: '))
-octal=int(input('valor oct: '))
+# hexa=int(input('valor hexa: '))
+# octal=int(input('valor oct: '))
+print(f"numero em hexadecimal, {hex(numero1)}")
+print(f"numero em octal,       {oct(numero1)}")
+print(f"numero em octal,       {bin(numero1)}")
