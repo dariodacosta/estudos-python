30fdbefc36cd480933cdcbc33b802d6b73072413
Dario Vitoriano <dariovitoriano2771@gmail.com>
2022-05-25 20:40:15 -0400

    new exercises

---
+++ new_q03.py
@@ -0,0 +1,8 @@
+metros=float(input('vlmet: '))
+decimetros=float(input('vldec: '))
+centimentros=float(input('vlcen: '))
+milimetros=float(input('vlmil: '))
+
+print(metros*10)
+print(metros*100)
+print(metros*1000)
