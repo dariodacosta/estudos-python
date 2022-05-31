0dbae33543969bbb61974817535339e28cdda292
Dario Vitoriano <dariovitoriano2771@gmail.com>
2022-05-31 18:15:48 -0400

    exe

---
+++ new_if_else_elif.py
@@ -0,0 +1,10 @@
+a = int(input("digite um numero"))
+b = int(input("digite outro numero"))
+if a > b:
+    print("a > b")
+elif a < b:
+    print("a < b")
+else:
+    print("b > a")
+
+print("fim do programa")
--- new_q03.py
+++ new_q03.py
@@ -1,7 +1,4 @@
 metros=float(input('vlmet: '))
-decimetros=float(input('vldec: '))
-centimentros=float(input('vlcen: '))
-milimetros=float(input('vlmil: '))
 
 print(metros*10)
 print(metros*100)
